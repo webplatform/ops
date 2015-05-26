0ae2207322de9508caf32751c33cd02471ba6a33
Renoir Boulanger <renoir@w3.org>
2015-05-26 13:35:08 -0400

    In workbench; disable MediaWiki clone; HEAVY
    Make sure that on a cloud environment we clone MediaWiki,
    but in Vagrant we'll do it manually.

--- salt-master/code.sh
+++ salt-master/code.sh
@@ -68,7 +68,6 @@ repos["compat"]="git@github.com:webplatform/compatibility-data.git"
 repos["docsprint-dashboard"]="git@github.com:webplatform/DocSprintDashboard.git"
 repos["www"]="git@github.com:webplatform/www.webplatform.org.git"
 repos["blog"]="git@github.com:webplatform/blog-service.git"
-repos["wiki"]="git@github.com:webplatform/mediawiki-core.git"
 
 options["buggenie"]="--branch webplatform-customizations --quiet"
 options["dabblet"]="--branch webplatform-customizations --quiet"
@@ -81,8 +80,14 @@ options["compat"]="--quiet"
 options["docsprint-dashboard"]="--quiet"
 options["www"]="--quiet"
 options["blog"]="--recurse-submodules --quiet"
-options["wiki"]="--branch wmf/1.25wmf15 --quiet"
-#options["wiki"]="--branch 1.24wmf16-wpd --recurse-submodules --quiet"
+
+if [ $IS_WORKBENCH == 0 ]; then
+  repos["wiki"]="git@github.com:webplatform/mediawiki-core.git"
+  options["wiki"]="--branch wmf/1.25wmf15 --quiet"
+  #options["wiki"]="--branch 1.24wmf16-wpd --recurse-submodules --quiet"
+else
+  echo "We will NOT clone MediaWiki, you will have to do it yourself. Its TOO HEAVY."
+fi
 
 #salt-call --local --log-level=quiet git.config_set setting_name=user.email setting_value="hostmaster@webplatform.org" is_global=True
 #salt-call --local --log-level=quiet git.config_set setting_name=user.name setting_value="WebPlatform Continuous Build user" is_global=True
@@ -119,18 +124,28 @@ for key in ${!repos[@]}; do
 done
 
 if [ $IS_WORKBENCH == 0 ]; then
-chown -R nobody:deployment /srv/code/
-find /srv/code -type f -exec chmod g+w {} \;
-find /srv/code -type d -exec chmod g+w {} \;
-find /srv/code -type d -exec chmod g+x {} \;
+  chown -R nobody:deployment /srv/code/
+  find /srv/code -type f -exec chmod g+w {} \;
+  find /srv/code -type d -exec chmod g+w {} \;
+  find /srv/code -type d -exec chmod g+x {} \;
 fi
 
 echo ""
 echo "Step 3 of 3 completed!"
 echo ""
-echo "Last step, install deployable code dependencies:"
-echo "  - As your normal user:"
-echo "      cd /srv/code/wiki/repo/mediawiki/; git submodule update --init --recursive"
-echo ""
-echo "... MediaWiki submodules are too heavy to run in this script."
+if [ $IS_WORKBENCH == 0 ]; then
+  echo "Last step, install deployable code dependencies:"
+  echo "  - As your normal user:"
+  echo "      cd /srv/code/wiki/repo/mediawiki/; git submodule update --init --recursive"
+  echo ""
+  echo "... MediaWiki submodules are too heavy to run in this script."
+else
+  echo "VAGRANT Workbench difference warning:"
+  echo ""
+  echo "Things should be very close to production, in this script we ignored file ownership chnages."
+  echo "It means that you might have to try in a deployment on OpenStack to make sure your rights dont break stuff."
+  echo ""
+  echo "ALSO; we didn't clone MediaWiki. Its too heavy."
+  echo "This part should be adressed differently or we should rework how we deploy MediaWiki."
+fi
 exit 0
--- vagrant-minions/README.md
+++ vagrant-minions/README.md
@@ -5,7 +5,8 @@ The *vagrant-workbench "salt master"* will take care of them.
 
 ## Use
 
-1. Make sure you booted and completed steps of [vagrant-workbench][../vagrant-workbench/] salt master
-2. Copy and edit **minions.yml**, the [minions.yml.dist][./minions.yml.dist] should be of help for the format to use.
-3. `vagrant up`
+1. Make sure you booted and completed steps of [vagrant-workbench](../vagrant-workbench/) salt master
+2. Copy and edit **minions.yml**, the [minions.yml.dist](./minions.yml.dist) should be of help for the format to use.
+3. Boot it up; `vagrant up`
+4. On the **salt** *vagrant workbench* VM, you'll see the machines waiting to be added to the salt master.
 
