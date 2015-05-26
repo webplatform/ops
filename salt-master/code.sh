555c0252df99d412c8c6181647d9c6d7e8e3a4db
Renoir Boulanger <renoir@w3.org>
2015-05-26 12:42:50 -0400

    salt-master/code.sh to consider differences between IN Vagrant and OUT

--- salt-master/code.sh
+++ salt-master/code.sh
@@ -19,6 +19,17 @@ set -e
 # 3. Cloning every webplatform.org software dependencies.
 #
 
+if [ -z "${RUNAS}" ]; then
+  echo "You must declare which user your VM initially has  e.g. RUNAS=vagrant bash code.sh"
+  exit 1
+fi
+
+if [ $SUDO_USER == "vagrant" ]; then
+  declare -r IS_WORKBENCH=1
+else
+  declare -r IS_WORKBENCH=0
+fi
+
 declare -r SALT_BIN=`which salt`
 
 if [ -z "${SALT_BIN}" ]; then
@@ -26,6 +37,7 @@ if [ -z "${SALT_BIN}" ]; then
   exit 1
 fi
 
+if [ $IS_WORKBENCH == 0 ]; then
 echo "Setting file ownership on salt master checkouts"
 chmod g+w /srv/code
 chown -R nobody:deployment /srv/{salt,pillar,private,code}
@@ -36,6 +48,7 @@ find /srv/salt -type d -exec chmod 775 {} \;
 find /srv/pillar -type d -exec chmod 775 {} \;
 find /srv/private -type d -exec chmod 770 {} \;
 chmod 755 /srv/salt/_grains/*.py
+fi
 
 echo "About to clone code..."
 
@@ -44,71 +57,36 @@ cd /srv/code
 declare -A repos
 declare -A options
 
-## SPECIAL CASES
-# n     accounts/
-#       certificates/         NOTE: not source controlled use salt and private!!
-#       gerrit/
-#       packages/
-# n     utilities/
-
-## CODE REPOS
-# x     blog/repo
-# .     bots/lumberjack/repo
-# x     buggenie/repo         @webplatform-customizations
-# .     campaign-bookmarklet
-# x     compat/repo
-# x     dabblet/repo          @webplatform-customizations
-# .     docsprint-dashboard
-# x     specs/repo                                                           requires out and archives too
-# x     wiki/repo/mediawiki   @1.24wmf16-wpd                                 requires ../cache/ ../settings.d/
-# x     www/repo              see https://source.webplatform.org/r/#/c/13/   requires ../out/ ../archives/
-# x     mailhub/repo
-# x     web25ee/repo
-# x     webat25/repo          Static version of ^
-
-repos["blog"]="git@github.com:webplatform/blog-service.git"
 repos["buggenie"]="git@github.com:webplatform/thebuggenie.git"
 repos["dabblet"]="git@github.com:webplatform/dabblet.git"
 repos["notes-server"]="git@github.com:webplatform/annotation-service.git"
 repos["bots"]="git@source.webplatform.org:pierc.git"
 repos["mailhub"]="git@source.webplatform.org:mailhub.git"
-repos["web25ee"]="git@source.webplatform.org:web25ee.git"
 repos["webat25"]="git@source.webplatform.org:webat25.git"
 repos["campaign-bookmarklet"]="git@github.com:webplatform/campaign-bookmarklet.git"
 repos["compat"]="git@github.com:webplatform/compatibility-data.git"
 repos["docsprint-dashboard"]="git@github.com:webplatform/DocSprintDashboard.git"
-repos["specs"]="git@github.com:webspecs/docs.git"
-repos["wiki"]="git@github.com:webplatform/mediawiki-core.git"
 repos["www"]="git@github.com:webplatform/www.webplatform.org.git"
+repos["blog"]="git@github.com:webplatform/blog-service.git"
+repos["wiki"]="git@github.com:webplatform/mediawiki-core.git"
 
-## Robin repository to deploy #TODO
-#repos["webspecs_bikeshed"]="git@github.com:webspecs/bikeshed.git"  branch webspecs
-#git@github.com:webspecs/assets.git"
-#git@github.com:webspecs/docs.git"
-#git@github.com:webspecs/the-index.git"
-#git@github.com:webspecs/publican.git
-
-options["blog"]="--recurse-submodules --quiet"
 options["buggenie"]="--branch webplatform-customizations --quiet"
 options["dabblet"]="--branch webplatform-customizations --quiet"
 options["notes-server"]="--quiet"
 options["bots"]="--quiet"
 options["mailhub"]="--quiet"
-options["web25ee"]="--quiet"
 options["webat25"]="--quiet"
 options["campaign-bookmarklet"]="--quiet"
 options["compat"]="--quiet"
 options["docsprint-dashboard"]="--quiet"
-options["specs"]="--quiet"
-#options["wiki"]="--branch 1.24wmf16-wpd --recurse-submodules --quiet"
-options["wiki"]="--branch wmf/1.25wmf15 --quiet"
 options["www"]="--quiet"
+options["blog"]="--recurse-submodules --quiet"
+options["wiki"]="--branch wmf/1.25wmf15 --quiet"
+#options["wiki"]="--branch 1.24wmf16-wpd --recurse-submodules --quiet"
 
 #salt-call --local --log-level=quiet git.config_set setting_name=user.email setting_value="hostmaster@webplatform.org" is_global=True
 #salt-call --local --log-level=quiet git.config_set setting_name=user.name setting_value="WebPlatform Continuous Build user" is_global=True
-
 #salt-call --local --log-level=quiet git.config_set setting_name=core.autocrlf setting_value=true is_global=True
-
 salt-call --local --log-level=quiet git.config_set setting_name=core.editor setting_value=vim is_global=True
 
 echo "We will be cloning code repositories:"
@@ -118,8 +96,10 @@ for key in ${!repos[@]}; do
       if [ ! -d "/srv/code/${key}/repo/mediawiki/.git" ]; then
         echo " * Cloning MediaWiki without dealing with gitmodules."
         mkdir -m 775 -p /srv/code/${key}/repo/mediawiki
-        chown -R renoirb:deployment /srv/code/${key}/repo/mediawiki
-        (salt-call --local --log-level=quiet git.clone /srv/code/${key}/repo/mediawiki ${repos[${key}]} opts="${options[${key}]}" user="renoirb")
+        if [ $IS_WORKBENCH == 0 ]; then
+          chown -R $RUNAS:deployment /srv/code/${key}/repo/mediawiki
+        fi
+        (salt-call --local --log-level=quiet git.clone /srv/code/${key}/repo/mediawiki ${repos[${key}]} opts="${options[${key}]}" user="$RUNAS")
         mkdir -m 775 /srv/code/${key}/repo/settings.d
       else
         echo " * Repo /srv/code/${key}/repo/mediawiki already cloned. Did nothing."
@@ -128,18 +108,22 @@ for key in ${!repos[@]}; do
       if [ ! -d "/srv/code/${key}/repo/.git" ]; then
         echo " * Cloning into /srv/code/${key}/repo"
         mkdir -m 775 -p /srv/code/${key}
-        chown -R renoirb:deployment /srv/code/${key}
-        (salt-call --local --log-level=quiet git.clone /srv/code/${key}/repo ${repos[${key}]} opts="${options[${key}]}" user="renoirb")
+        if [ $IS_WORKBENCH == 0 ]; then
+            chown -R $RUNAS:deployment /srv/code/${key}
+        fi
+        (salt-call --local --log-level=quiet git.clone /srv/code/${key}/repo ${repos[${key}]} opts="${options[${key}]}" user="$RUNAS")
       else
         echo " * Repo in /srv/code/${key}/repo already cloned. Did nothing."
       fi
     fi
 done
 
+if [ $IS_WORKBENCH == 0 ]; then
 chown -R nobody:deployment /srv/code/
 find /srv/code -type f -exec chmod g+w {} \;
 find /srv/code -type d -exec chmod g+w {} \;
 find /srv/code -type d -exec chmod g+x {} \;
+fi
 
 echo ""
 echo "Step 3 of 3 completed!"
